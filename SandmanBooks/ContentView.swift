e84683d63da85b3a847542c71d223c380db26917
Valerie <valeriestgermain@yahoo.com>
2024-03-27 20:57:24 -0400

    Initial Commit

---
+++ SandmanBooks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SandmanBooks
+//
+//  Created by Valerie St. Germain on 3/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SandmanBooks/SandmanBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandmanBooksApp.swift
+//  SandmanBooks
+//
+//  Created by Valerie St. Germain on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SandmanBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
